eca6b956475017b9c4bb46f25872a2395804975d
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-14 15:28:03 +0000

    initial commit

---
+++ Lesson 8 Activity 2.py
@@ -0,0 +1,6 @@
+numerator = int(input("Enter a numerator: "))
+denominator = int(input("Enter a denominator: "))
+if (numerator%denominator == 0):
+    print ("divisible")
+else:
+    print ("not divisible")
