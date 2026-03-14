f21312960b37a18d522cfc8ecd481ac859a74637
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-14 15:34:02 +0000

    initial commit

---
+++ Lesson 8 Activity 3.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrong_num = 36
+correct_num = 56
+total_num = 40
+sum1 = mean1 * total_num
+print ("The sum of all the numbers is: ", sum1)
+sum2 = sum1-(wrong_num-correct_num)
+print ("The correct sum of all the numbers is: ", sum2)
+mean2 = sum2/total_num
+print ("The total mean is: ", mean2)
