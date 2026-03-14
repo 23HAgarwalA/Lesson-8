f92d71d2990f4a707aae0a70f4a0acb4eb0f3383
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-14 15:23:58 +0000

    initial commit

---
+++ Lesson 8 Activity 1.py
@@ -0,0 +1,15 @@
+a = 6
+b = 9
+c = 15
+d = 3
+e = 324
+print ("a = 6, b = 9, c = 15, d = 3, e = 324")
+f = (a+b) // b **c / d + e 
+print ("The value of (a+b) // b **c / d + e is", f)
+
+name = "Bob"
+age = 45
+if name == "John" and "Jack" or age <= 30 or age == 45:
+    print ("Hello, how are you?")
+else:
+    print ("See you later")
