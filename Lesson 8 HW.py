c0c4eda5cd0eab96edd9199224b2c9cfe1eaf2c6
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-29 14:55:35 +0100

    initial commit

---
+++ Lesson 8 HW.py
@@ -0,0 +1,8 @@
+a = input("Enter the first value(a): ")
+b =  input("Enter the first value(b): ")
+c =  input("Enter the first value(c): ")
+print ("\nBefore swapping:")
+print ("a = ", a, "b = ", b, "c = ", c)
+a,b,c = c,a,b
+print("\nAfter swapping:")
+print ("a = ",a, "b = ",b, "c = ",c)
